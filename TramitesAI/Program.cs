e0d088415f73c38d42baee4cce60ff5e79dfb8b8
Denise Eichenblat <denise.eichenblat@extendeal.com>
2024-04-19 15:47:06 -0300

    ilustrative images validation

--- TramitesAI/Program.cs
+++ TramitesAI/Program.cs
@@ -1,25 +1,63 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Tesseract;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapControllers();
+
+// Main method to perform OCR
+static void PerformOCR()
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Inicializar el motor de Tesseract
+    try
+    {
+        using (var engine = new TesseractEngine(@"./tessdata", "spa", EngineMode.Default))
+        {
+            // OK: otros_ejemplos2.png - 12346-imagen00231.jpg - 12345-imagen00231.jpeg -
+            // Failed - otros_ejemplos1.pdf - 12347-imagen00231.docx
+            using (var img = Pix.LoadFromFile("./images/12345-foto1.jpg")) 
+            {
+                    //Console.WriteLine("IMAGE", img);
+                    using (var page = engine.Process(img))
+                    {
+                        var text = page.GetText();
+                        bool contieneLetras = text.Any(char.IsLetter); //Para verificar que no sea una imagen ilustrativa
+
+                        if (contieneLetras)
+                        {
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence()); //CLIENTES: Ya desde este dato filtramos respuesta?
+                            Console.WriteLine("Text (GetText): {0}", text);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Imagen ilustrativa");
+                        }
+                        
+                        //Console.WriteLine("Text (iterator):");
+                    }
+                }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Se lanzó una excepción: {ex.Message}");
+    }
+
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+PerformOCR();
 
 app.Run();
