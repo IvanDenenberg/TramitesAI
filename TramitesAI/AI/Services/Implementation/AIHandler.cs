88ca77543af3ab7d0b612c9b035ce1529d64a2f4
Ivan Denenberg <ivan.denenberg@gmail.com>
2024-04-19 18:44:36 -0300

    feat: refactor of AIHandler and its interface also adding first method

---
+++ TramitesAI/AI/Services/Implementation/AIHandler.cs
@@ -0,0 +1,15 @@
+﻿using TramitesAI.AI.Domain.Dto;
+using TramitesAI.AI.Services.Interfaces;
+using TramitesAI.Business.Domain.Dto;
+using TramitesAI.Repository.Domain.Dto;
+
+namespace TramitesAI.AI.Services.Implementation
+{
+    public class AIHandler : IAIHandler
+    {
+        public AnalyzedInformationDTO ProcessInfo(List<FileStream> files, List<BusinessRulesDTO> rules, RequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TramitesAI/AI/Services/Interfaces/IAIHandler.cs
@@ -0,0 +1,11 @@
+﻿using TramitesAI.AI.Domain.Dto;
+using TramitesAI.Business.Domain.Dto;
+using TramitesAI.Repository.Domain.Dto;
+
+namespace TramitesAI.AI.Services.Interfaces
+{
+    public interface IAIHandler
+    {
+        AnalyzedInformationDTO ProcessInfo(List<FileStream> files, List<BusinessRulesDTO> rules, RequestDTO requestDTO);
+    }
+}
