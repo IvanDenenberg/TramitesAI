961b726e8b866350ef874093a632321f8e92ddf9
Ivan Denenberg <ivan.denenberg@gmail.com>
2024-04-19 01:23:57 -0300

    feat: adding generic interface for repository and one entity

---
+++ TramitesAI/Repository/Interfaces/IRepositoryService.cs
@@ -0,0 +1,14 @@
+﻿namespace TramitesAI.Repository.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IRepositoryService<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(string id);
+        Task Create(TEntity entity);
+        Task Update(string id, TEntity entity);
+        Task Delete(string id);
+    }
+}
