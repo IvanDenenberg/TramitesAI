e1a3ae0548fb3cf0004725fbd85db9249d0cf996
Ivan Denenberg <ivan.denenberg@gmail.com>
2024-04-19 01:26:53 -0300

    feat: adding basic endpoints

--- TramitesAI/Controllers/ProcessController.cs
+++ TramitesAI/Controllers/ProcessController.cs
@@ -1,6 +1,35 @@
-﻿namespace TramitesAI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TramitesAI.Business.Domain.Dto;
+using TramitesAI.Business.Services.Interfaces;
+
+namespace TramitesAI.Controllers
 {
-    public class ProcessController
+    // The route of this controller will have the suffix "api/process"
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessController : ControllerBase
     {
+        private IBusinessService _businessService;
+
+        public ProcessController(IBusinessService businessService)
+        {
+            _businessService = businessService;
+        }
+
+        [HttpPost]
+        public IActionResult Process([FromBody] RequestDTO request)
+        {
+            ResponseDTO response = _businessService.Process(request);
+            //TODO Change return
+            return Ok(response);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetByMsgId([FromRoute] string id) 
+        {
+            ResponseDTO response = _businessService.GetById(id);
+            //TODO Change return
+            return Ok(response);
+        }
     }
 }
