9da917dd93d83c38b805302f26c3a9a5ff9fd0bb
Denise Eichenblat <denise.eichenblat@extendeal.com>
2024-04-19 17:11:45 -0300

    pdf files

--- TramitesAI/Program.cs
+++ TramitesAI/Program.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Drawing;
-using System.IO;
-using System.Net.Http;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Tesseract;
+using IronPdf;
+using IronOcr;
+
 
 
 
@@ -26,29 +23,40 @@ static void PerformOCR()
     {
         using (var engine = new TesseractEngine(@"./tessdata", "spa", EngineMode.Default))
         {
-            // OK: otros_ejemplos2.png - 12346-imagen00231.jpg - 12345-imagen00231.jpeg -
+            // OK: otros_ejemplos2.png - 12346-imagen00231.jpg - 12345-imagen00231.jpeg - 12345-foto1.jpg
             // Failed - otros_ejemplos1.pdf - 12347-imagen00231.docx
-            using (var img = Pix.LoadFromFile("./images/12345-foto1.jpg")) 
-            {
-                    //Console.WriteLine("IMAGE", img);
-                    using (var page = engine.Process(img))
-                    {
-                        var text = page.GetText();
-                        bool contieneLetras = text.Any(char.IsLetter); //Para verificar que no sea una imagen ilustrativa
+            string filePath = "./images/otros_ejemplos1.pdf";
+            //Seguro haya que cambiar esta lógica en base a cómo recibimos el archivo
+            bool terminaEnPDF = filePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase); 
 
-                        if (contieneLetras)
+            if (terminaEnPDF)
+            {
+                Console.WriteLine("El archivo termina en .pdf");
+            }
+            else
+            {
+                  using (var img = Pix.LoadFromFile(filePath)) // tamb me va a permitir probar con archivos de más de una página
+                  {
+                        //Console.WriteLine("IMAGE", img);
+                        using (var page = engine.Process(img))
                         {
-                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence()); //CLIENTES: Ya desde este dato filtramos respuesta?
-                            Console.WriteLine("Text (GetText): {0}", text);
+                            var text = page.GetText();
+                            bool contieneLetras = text.Any(char.IsLetter); //Para verificar que no sea una imagen ilustrativa
+
+                            if (contieneLetras)
+                            {
+                                Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence()); //CLIENTES: Ya desde este dato filtramos respuesta?
+                                Console.WriteLine("Text (GetText): {0}", text);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Imagen ilustrativa");
+                            }
+
+                            //Console.WriteLine("Text (iterator):");
                         }
-                        else
-                        {
-                            Console.WriteLine("Imagen ilustrativa");
-                        }
-                        
-                        //Console.WriteLine("Text (iterator):");
-                    }
-                }
+                  }
+            }
         }
     }
     catch (Exception ex)
