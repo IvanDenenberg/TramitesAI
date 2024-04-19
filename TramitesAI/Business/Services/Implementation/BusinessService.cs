3fde03dffa99538b3de6649407d6d5b6d4acc8c8
Ivan Denenberg <ivan.denenberg@gmail.com>
2024-04-19 01:25:18 -0300

    feat: adding interface and implementation of service

---
+++ TramitesAI/Business/Services/Implementation/BusinessService.cs
@@ -0,0 +1,21 @@
+﻿using TramitesAI.Business.Domain.Dto;
+using TramitesAI.Business.Services.Interfaces;
+using TramitesAI.Repository.Interfaces;
+
+namespace TramitesAI.Business.Services.Implementation
+{
+    public class BusinessService : IBusinessService
+    {
+        private IRepositoryService<> repositoryService;
+
+        public ResponseDTO GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseDTO Process(RequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TramitesAI/Business/Services/Interfaces/IBusinessService.cs
@@ -0,0 +1,10 @@
+﻿using TramitesAI.Business.Domain.Dto;
+
+namespace TramitesAI.Business.Services.Interfaces
+{
+    public interface IBusinessService
+    {
+        public ResponseDTO Process(RequestDTO requestDTO);
+        public ResponseDTO GetById(string id);
+    }
+}
