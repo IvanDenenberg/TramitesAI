ef4b40bd5a256f7fc3a8947559456786b4b247f7
Ivan Denenberg <ivan.denenberg@gmail.com>
2024-04-25 22:03:32 -0300

    feat: adding implemenation of interface, deleting Startup(deprecated) and addding dependency injection in program

--- TramitesAI/Program.cs
+++ TramitesAI/Program.cs
@@ -1,3 +1,11 @@
+using TramitesAI.AI.Services.Implementation;
+using TramitesAI.AI.Services.Interfaces;
+using TramitesAI.Business.Services.Implementation;
+using TramitesAI.Business.Services.Interfaces;
+using TramitesAI.Repository.Domain.Dto;
+using TramitesAI.Repository.Implementations;
+using TramitesAI.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IBusinessService, BusinessService>();
+builder.Services.AddSingleton<IAIHandler, AIHandler>();
+builder.Services.AddSingleton<IAIInformationExtractor, TesseractService>();
+builder.Services.AddSingleton<IAIAnalyzer, TensorFlowService>();
+builder.Services.AddSingleton<IRepository<ProcessedCasesDTO>, ProcessedCasesRepository>();
+builder.Services.AddSingleton<IFileSearcher, GoogleDriveSearcherService>();
 
 var app = builder.Build();
 
