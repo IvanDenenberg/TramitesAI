5531fba3c65536013713cd3f9d52f76529d22950
Ariel Solla <arisolla@hotmail.com>
2024-05-01 19:28:09 -0300

    borrado de bin y refactorizacion de tesseract

--- TramitesAI/AI/Services/Implementation/TesseractService.cs
+++ TramitesAI/AI/Services/Implementation/TesseractService.cs
@@ -28,32 +28,35 @@ namespace TramitesAI.AI.Services.Implementation
                 // Inicialización del motor de Tesseract
                 using (var engine = new TesseractEngine(@"./tessdata", "spa", EngineMode.Default))
                 {
-                    //string filePath = "./images/otros_ejemplos1.pdf";
-
-
-                    // Verificar si el archivo es un PDF
-                    bool esPDF = filePath.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase);
+                    // Verificar si el archivo es un PDF (por ejemplo, mediante la extensión del nombre de archivo)
+                    bool esPDF = file is FileStream && ((FileStream)file).Name.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase);
 
                     List<string> pngFilePaths = new List<string>();
 
                     if (esPDF)
                     {
-                        // Convertir el PDF a PNG
-                        pngFilePaths = ConvertirPDFAPNG(filePath, engine);
+                        // Convertir el PDF a PNG y obtener las rutas de archivo de las imágenes PNG generadas
+                        //pngFilePaths = ConvertirPDFAPNG(file, engine);
+                        ConvertirPDFAPNG(file, engine, pngFilePaths);
+
                     }
                     else
                     {
-                        // Agregara el archivo original
-                        pngFilePaths.Add(filePath);
+                        string tempFilePath = Path.GetTempFileName();
+                        using (var tempFileStream = File.OpenWrite(tempFilePath))
+                        {
+                            file.CopyTo(tempFileStream);
+                        }
+                        pngFilePaths.Add(tempFilePath);
                     }
 
+                    // Iterar sobre las rutas de archivo de las imágenes PNG generadas o del Stream original
                     int i = 1;
                     foreach (var pngFilePath in pngFilePaths)
                     {
                         ProcesarArchivo(pngFilePath, engine);
-                        Console.WriteLine("Pagina " + i);
+                        Console.WriteLine("Página " + i);
                         i++;
-
                     }
                 }
             }
@@ -64,24 +67,52 @@ namespace TramitesAI.AI.Services.Implementation
             return new ExtractedInfoDTO();
         }
 
-        static List<string> ConvertirPDFAPNG(string filePath, TesseractEngine engine)
+        static void ConvertirPDFAPNG(Stream fileStream, TesseractEngine engine, List<string> pngFilePaths)
         {
-            List<string> pngFilePaths = new List<string>();
+            //List<string> pngFilePaths = new List<string>();
 
-            string pdfFileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
-            byte[] pdfFileAsByte = File.ReadAllBytes(filePath);
-            List<byte[]> pngFilesAsBytes = Freeware.Pdf2Png.ConvertAllPages(pdfFileAsByte);
-            int pageCounter = 0;
-            foreach (byte[] onePngAsBytes in pngFilesAsBytes)
+            try
             {
-                pageCounter++;
-                string pngFilePath = Path.Combine(@"./images/PdfsToPng/", $"{pdfFileNameWithoutExtension}_{pageCounter}.png");
-                File.WriteAllBytes(pngFilePath, onePngAsBytes);
-                pngFilePaths.Add(pngFilePath);
+                byte[] pdfFileAsByte;
+
+                // Verificar si el Stream es un FileStream y leer los datos del archivo
+                if (fileStream is FileStream)
+                {
+                    pdfFileAsByte = File.ReadAllBytes(((FileStream)fileStream).Name);
+                }
+                else
+                {
+                    // El Stream no es un FileStream, leer los datos del Stream
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        fileStream.CopyTo(memoryStream);
+                        pdfFileAsByte = memoryStream.ToArray();
+                    }
+                }
+
+                // Convertir el PDF a PNG
+                List<byte[]> pngFilesAsBytes = Freeware.Pdf2Png.ConvertAllPages(pdfFileAsByte);
+
+                string pdfFileNameWithoutExtension = Path.GetFileNameWithoutExtension(((FileStream)fileStream).Name);
+                int pageCounter = 0;
+
+                // Guardar cada página convertida como un archivo PNG y agregar la ruta al listado
+                foreach (byte[] onePngAsBytes in pngFilesAsBytes)
+                {
+                    pageCounter++;
+                    string pngFilePath = Path.Combine(@"./images/PdfsToPng/", $"{pdfFileNameWithoutExtension}_{pageCounter}.png");
+                    File.WriteAllBytes(pngFilePath, onePngAsBytes);
+                    pngFilePaths.Add(pngFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se lanzó una excepción durante la conversión de PDF a PNG: {ex.Message}");
             }
 
-            return pngFilePaths;
+            //return pngFilePaths;
         }
+
 
         static void ProcesarArchivo(string filePath, TesseractEngine engine)
         {
