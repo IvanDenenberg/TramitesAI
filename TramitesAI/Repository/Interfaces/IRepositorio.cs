e9d618a1a3d618014be96854b694ae3e7170b7be
Ivan Denenberg <ivan.denenberg@gmail.com>
2024-05-17 21:09:53 -0300

    fix: readapting to spanish

---
+++ TramitesAI/Repository/Interfaces/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿namespace TramitesAI.Repository.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IRepositorio<TEntity>
+    {
+        Task<IEnumerable<TEntity>> LeerTodos();
+        Task<TEntity> LeerPorId(int id);
+        Task<int> Crear(TEntity entidad);
+        Task<TEntity> Modificar(TEntity entidad);
+        Task<TEntity> Borrar(int id);
+    }
+}
